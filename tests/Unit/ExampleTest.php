33eb6467153bb01d930a458857c919e72b151e3b
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-05 14:14:21 +0900

    Add fizzbuzz parturn

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,28 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use function Hidenari\HelperSample\fizzBuzz;
+
+test('fizzBuzz with parameter', function () {
+
+    $data = [
+        "1"  => 1,
+        "2"  => 2,
+        "3"  => "fizz",
+        "4"  => 4,
+        "5"  => "buzz",
+        "6"  => "fizz",
+        "7"  => 7,
+        "8"  => 8,
+        "9"  => "fizz",
+        "10" => "buzz",
+        "11" => 11,
+        "12" => "fizz",
+        "13" => 13,
+        "14" => 14,
+        "15" => "fizzbuzz",
+    ];
+
+    foreach ($data as $key => $value) {
+        expect(fizzBuzz($key) === $value)->toBeTrue();
+    }
 });
