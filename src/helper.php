a31e3ff6cd151ad3c51695cc77038d4a0cfe7e48
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-20 12:21:19 +0900

    Mod business logic

--- src/helper.php
+++ src/helper.php
@@ -9,7 +9,7 @@ function fizzBuzz(int|float|bool $number): int|string
     }
 
     return match (true) {
-        (int) $number % 15 === 0 => 'fizzbuzz',
+        (int) $number % 3 === 0 && (int) $number % 5 === 0 => 'fizzbuzz',
         (int) $number % 3 === 0 => 'fizz',
         (int) $number % 5 === 0 => 'buzz',
         default => (int) $number,
